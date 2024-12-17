7f30cc550ffd3061a72382bfaaab534b20b2d291
Yogi <rawat.yogesh@gmail.com>
2024-12-17 00:34:13 -0500

    Practicing git

--- src/main/java/se/roger/App_new.java
+++ src/main/java/se/roger/App_new.java
@@ -22,6 +22,16 @@ public class App {
         log.debug("DEBUG: Request on ping endpoint22");
         return "pong";
     }
+
+    @RequestMapping(value = "/loglevel/{loglevel}", method = RequestMethod.POST)
+    public String loglevel(@PathVariable("loglevel") String logLevel, @RequestParam(value="package") String packageName) throws Exception {
+        log.info("Log level: " + logLevel);
+        log.info("Package name: " + packageName);
+        log.info("Package name again: " + packageName);
+        String retVal = setLogLevel(logLevel, packageName);
+        return retVal;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
